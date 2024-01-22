5116aa12a0cdb9d265c6a09dda085981e3d7739a
VictorJacobi <alabivictor015@gmail.com>
2024-01-22 11:43:04 +0100

    initial commit

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -20,8 +20,14 @@ class MyHomeScreen extends StatefulWidget {
 class _MyHomeScreenState extends State<MyHomeScreen> {
   final dbRef = FirebaseDatabase.instance.ref();
   int batteryValue = 0;
-  Future<void> writeData() async {
-    await dbRef.child("LightState").set({"switch": !value});
+  Future<bool> writeData() async {
+    try{
+      await dbRef.child("LightState").set({"switch": !value});
+      return true;
+    }catch(e){
+      log('Error: $e');
+    }
+   return false;
   }
 
   Future<void> readData() async {
@@ -42,80 +48,95 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-        ),
-        backgroundColor: !value ? onColor : offColor,
-        onPressed: () {
-          setState(() {
-            writeData();
-            value = !value;
-          });
-        },
-        label: Center(
-          child: Text(
-            value == true ? 'OFF' : 'ON',
-            style: const TextStyle(
-              fontSize: 30,
+    return StreamBuilder<DatabaseEvent>(
+      stream: dbRef.child("BatteryLevel/level").onValue,
+      builder: (context, snapshot) {
+        log(('snapshot: ${snapshot.data?.snapshot.value}'));
+        int? level = snapshot.data?.snapshot.value as int?;
+
+        if(snapshot.data==null){
+          return const Scaffold(body: SizedBox());
+        }
+        return Scaffold(
+          floatingActionButton: FloatingActionButton.extended(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+            ),
+            backgroundColor: !value ? onColor : Colors.blue,
+            onPressed: () async {
+              await writeData().then((result) {
+                if(result){
+                  setState(() {
+                    value = !value;
+                  });
+                }
+              });
+
+            },
+            label: Center(
+              child: Text(
+                value == true ? 'OFF' : 'ON',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text(
-          'IOT APP',
-          style: TextStyle(
-            // color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+            title: const Text(
+              'IOT APP',
+              style: TextStyle(
+                // color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
           ),
-        ),
-      ),
-      body: SafeArea(
-        child: RefreshIndicator(
-          onRefresh: () async {
-            await readData();
-          },
-          child: SingleChildScrollView(
-            physics: const AlwaysScrollableScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: MediaQuery.sizeOf(context).height - 80,
-                  child: Center(
-                    child: SizedBox(
-                      width: MediaQuery.sizeOf(context).width * 0.7,
-                      height: MediaQuery.sizeOf(context).height / 3,
+          body: SafeArea(
+            child: RefreshIndicator(
+              onRefresh: () async {
+                await readData();
+              },
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.sizeOf(context).height - 80,
                       child: Center(
-                        child: CircleProgressBar(
-                          foregroundColor: Colors.blue,
-                          backgroundColor: Colors.black12,
-                          value: (3.3 * batteryValue / (4093 * 0.138 * 24.8)),
-                          strokeWidth: 20,
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width * 0.7,
+                          height: MediaQuery.sizeOf(context).height / 3,
                           child: Center(
-                            child: AnimationCount(
-                              count:
-                                  (3.3 * batteryValue / (4093 * 0.138 * 24.8)),
-                              unit: '%',
-                              unitScaleFactor: 4,
-                              duration: const Duration(milliseconds: 500),
+                            child: CircleProgressBar(
+                              foregroundColor: Colors.blue,
+                              backgroundColor: Colors.black12,
+                              value: (3.3 * (level!) / (4093 * 0.138 * 24.8)),
+                              strokeWidth: 20,
+                              child: Center(
+                                child: AnimationCount(
+                                  count:
+                                      (3.3 * level / (4093 * 0.138 * 24.8)),
+                                  unit: '%',
+                                  unitScaleFactor: 4,
+                                  duration: const Duration(milliseconds: 500),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
